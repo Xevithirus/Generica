54e9290a62b91ff0d6662355062f8a9f06afe393
Xevithirus <xevithirus@gmail.com>
2024-07-28 15:02:13 +1200

    Refactored menu system to move freely between all locations as intended

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,133 @@
 import React, { useState } from 'react';
-// import Menu from './Menu.jsx';
-// import Game from './Game.jsx';
 import Sidebar from './common/Sidebar';
-import WorldLocation from './WorldLocation';
+import LocationScreen from './LocationScreen';
+import { WorldData } from './WorldData';
 import './App.css';
 
 const App = () => {
-  // const [gameState, setGameState] = useState('menu'); // 'menu' or 'game'
-  const [currentLocation, setCurrentLocation] = useState('darda');
+  const [currentRegion, setCurrentRegion] = useState('faldor');
+  const [currentArea, setCurrentArea] = useState('darda');
+  const [currentLocalPosition, setCurrentLocalPosition] = useState(null);
+  const [currentActivity, setCurrentActivity] = useState(null);
+  const [showPopup, setShowPopup] = useState(false);
+  const [popupList, setPopupList] = useState([]);
+  const [popupType, setPopupType] = useState('');
 
-  // const startGame = () => {
-  //   setGameState('game');
-  // };
+  const currentRegionData = WorldData[currentRegion];
+  const currentAreaData = currentRegionData?.areas[currentArea];
+  const currentLocalPositionData = currentAreaData?.localPositions[currentLocalPosition];
+  const currentActivityData = currentLocalPositionData?.activities[currentActivity];
 
-  // const quitGame = () => {
-  //   setGameState('menu');
-  // };
+  const mainImage = currentActivityData
+    ? currentActivityData.image
+    : currentLocalPositionData?.image || currentAreaData?.image || currentRegionData.image;
+  const mainTitle = currentActivityData
+    ? currentActivityData.name
+    : currentLocalPositionData?.name || currentAreaData?.name || currentRegionData.name;
+  const mainDescription = currentActivityData
+    ? currentActivityData.description
+    : currentLocalPositionData?.description || currentAreaData?.description || currentRegionData.description;
+
+  const handlePopupToggle = (type, list) => {
+    if (showPopup && popupType === type) {
+      setShowPopup(false);
+      setPopupType('');
+    } else {
+      setPopupList(list);
+      setShowPopup(true);
+      setPopupType(type);
+    }
+  };
+
+  const handleTravel = () => {
+    const connectedAreas = currentAreaData.connectedAreas;
+    const connectedAreaNames = connectedAreas.map(areaKey => currentRegionData.areas[areaKey]?.name);
+    handlePopupToggle('travel', connectedAreaNames);
+  };
+
+  const handleLocal = () => {
+    const localPositionKeys = Object.keys(currentAreaData.localPositions);
+    const localPositionNames = localPositionKeys.map(posKey => currentAreaData.localPositions[posKey]?.name);
+    handlePopupToggle('local', localPositionNames.filter(posName => posName !== currentLocalPosition));
+  };
+
+  const handleActivities = () => {
+    const activityKeys = Object.keys(currentLocalPositionData.activities);
+    const activityNames = activityKeys.map(actKey => currentLocalPositionData.activities[actKey]?.name);
+    handlePopupToggle('activities', activityNames);
+  };
+
+  const handleSelect = (item) => {
+    setShowPopup(false);
+    setPopupType('');
+    
+    const selectedKey = (() => {
+      if (popupType === 'activities') {
+        return Object.keys(currentLocalPositionData.activities).find(key => currentLocalPositionData.activities[key].name === item);
+      } else if (popupType === 'local') {
+        return Object.keys(currentAreaData.localPositions).find(key => currentAreaData.localPositions[key].name === item);
+      } else {
+        return Object.keys(currentRegionData.areas).find(key => currentRegionData.areas[key].name === item);
+      }
+    })();
+
+    if (popupType === 'activities') {
+      setCurrentActivity(selectedKey);
+    } else if (popupType === 'local') {
+      setCurrentActivity(null);
+      setCurrentLocalPosition(selectedKey);
+    } else {
+      setCurrentActivity(null);
+      setCurrentLocalPosition(null);
+      setCurrentArea(selectedKey);
+    }
+  };
+
+  const handleClickOutsidePopup = (e) => {
+    if (showPopup && !e.target.closest('.popup') && !e.target.closest('.travel-button') && !e.target.closest('.local-button') && !e.target.closest('.activities-button')) {
+      setShowPopup(false);
+      setPopupType('');
+    }
+  };
 
   return (
-    // <div>
-    //   {gameState === 'menu' ? (
-    //     <Menu startGame={startGame} />
-    //   ) : (
-    //     <Game quitGame={quitGame} />
-    //   )}
-    // </div>
-    <div className="app">
-      <Sidebar setCurrentLocation={setCurrentLocation} />
+    <div className="app" onClick={handleClickOutsidePopup}>
+      <Sidebar
+        setCurrentRegion={setCurrentRegion}
+        setCurrentArea={setCurrentArea}
+        setCurrentLocalPosition={setCurrentLocalPosition}
+        setCurrentActivity={setCurrentActivity}
+      />
       <div className="location-screen">
         <div className="main-image">
-          <img src={locations[currentLocation].image} alt={locations[currentLocation].name} />
+          <img src={mainImage} alt={mainTitle} />
         </div>
-        <WorldLocation
-          location={locations[currentLocation]}
-          locations={locations}
-          setCurrentLocation={setCurrentLocation}
+        <LocationScreen
+          region={currentRegionData}
+          area={currentAreaData}
+          localPosition={currentLocalPositionData}
+          activity={currentActivityData}
+          handleTravel={handleTravel}
+          handleLocal={handleLocal}
+          handleActivities={handleActivities}
+          setCurrentArea={setCurrentArea}
+          setCurrentLocalPosition={setCurrentLocalPosition}
+          setCurrentActivity={setCurrentActivity}
         />
       </div>
+      {showPopup && (
+        <div className="popup">
+          <ul>
+            {popupList.map(item => (
+              <li key={item} onClick={() => handleSelect(item)}>
+                {item}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
 
 export default App;
-
-// ----------------------------- LOCATIONS ---------------------------------
-const locations = {
-  // FALDOR REGION
-  darda: {
-    name: 'Darda',
-    description: 'Welcome to Darda! This quaint little fishing village is nestled in the North-Eastern edge of Faldor. Only the most well-travelled know of its incredible seafood and kind townfolk.',
-    image: './images/darda.png',
-    nearby: ['theValley'],
-  },
-  theValley: {
-    name: 'The Valley',
-    description: "You've arrived at The Valley. Hidden from the world within an unbroken wall of mountains, it is a peaceful green paradise. The wildlife has been well preserved and many interesting animals roam its grassy plains.",
-    image: './images/the-valley.png',
-    nearby: ['darda', 'coralCove', 'thalenPass'],
-  },
-  coralCove: {
-    name: 'Coral Cove',
-    description: "You've arrived at Coral Cove. A beautiful sandy beach boardered by impressive cliff walls.",
-    image: './images/coral-cove.png',
-    nearby: ['theValley', 'blusterBluffs'],
-  },
-  blusterBluffs: {
-    name: 'Bluster Bluffs',
-    description: "You've climbed Bluster Bluffs. Overlooking the Coral Cove, it provides a great view of the sea!",
-    image: './images/bluster-bluffs.png',
-    nearby: ['coralCove'],
-  },
-  thalenPass: {
-    name: 'Thalen Pass',
-    description: "You've arrived at Thalen Pass. This old mining tunnel used to provide throughfare to the world beyond the Westridge Mountains. Alas, it appears to have been sealed for saftey.",
-    image: './images/thalen-pass.png',
-    nearby: ['theValley'],
-  },
-};
---
+++ src/LocationScreen.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const LocationScreen = ({ region, area, localPosition, activity, handleTravel, handleLocal, handleActivities, setCurrentArea, setCurrentLocalPosition, setCurrentActivity }) => {
+  return (
+    <div className="world-location">
+      <h2 className="location-name">{activity ? activity.name : (localPosition?.name || area?.name || region.name)}</h2>
+      <p className="location-description">{activity ? activity.description : (localPosition?.description || area?.description || region.description)}</p>
+      <div className="button-container">
+        {!localPosition && !activity && (
+          <>
+            <button className="travel-button" onClick={handleTravel}>Travel</button>
+            <button className="enter-button" onClick={() => setCurrentLocalPosition(Object.keys(area.localPositions)[0])}>Enter</button>
+          </>
+        )}
+        {localPosition && !activity && (
+          <>
+            <button className="local-button" onClick={handleLocal}>Local</button>
+            <button className="activities-button" onClick={handleActivities}>Activities</button>
+            <button className="leave-button" onClick={() => { setCurrentLocalPosition(null); setCurrentActivity(null); }}>Leave Area</button>
+          </>
+        )}
+        {activity && (
+          <button className="return-button" onClick={() => setCurrentActivity(null)}>Return</button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default LocationScreen;
